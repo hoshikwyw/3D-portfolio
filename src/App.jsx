72d9372709d621e40bfdffb31970149827e72ab8
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-11-27 22:43:45 +0630

    router add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      app
-    </div>
-  )
-}
+    <main className=" bg-slate-300/20">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={"Home"} />
+          <Route path="/about" element={"About"} />
+          <Route path="/projects" element={"Projects"} />
+          <Route path="/contact" element={"Contact"} />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
+};
 
-export default App
+export default App;
