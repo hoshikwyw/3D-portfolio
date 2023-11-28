d15a9147b9c6d7e4b4207ed8cd21e25ddae2730d
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-11-28 22:56:08 +0630

    island add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,13 @@ const Home = () => {
     <section className=" w-full h-screen relative">
       {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center"></div> */}
       <Canvas className=' w-full h-screen bg-transparent' camera={{near:0.1, far: 1000}}>
-        <Suspense fallback={<Loader />}></Suspense>
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+        </Suspense>
       </Canvas>
     </section>
   )
