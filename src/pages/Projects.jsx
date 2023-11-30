22cd31c1c73d4d3e70f75272b638ac0b0266c9b0
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-11-30 20:59:01 +0630

    sa tar bar pyar

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Projects = () => {
   return (
     <div>
-      Projects
+      <h2>hi this is projects page</h2>
+      <p>in this page i will show you my awesome projects heehee</p>
     </div>
   )
 }
